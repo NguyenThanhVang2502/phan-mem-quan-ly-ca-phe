84c3b4ac7c00aba93457076a4c1e5419b090f3a2
thanh_vang <nguyenvang25022001@gmail.com>
2023-08-21 00:01:42 +0700

    first commit

---
+++ QuanLyQuanCafe/DAO/CategoryDAO.cs
@@ -0,0 +1,81 @@
+﻿using QuanLyQuanCafe.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace QuanLyQuanCafe.DAO
+{
+    public class CategoryDAO
+    {
+        private static CategoryDAO instance;
+
+        public static CategoryDAO Instance
+        {
+            get { if (instance == null)instance = new CategoryDAO(); return CategoryDAO.instance; }
+            private set { CategoryDAO.instance = value; }
+        }
+
+        private CategoryDAO() { }
+
+        public List<Category> GetListCategory()
+        {
+            List<Category> list = new List<Category>();
+
+            string query = "select * from FoodCategory";
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                Category category = new Category(item);
+                list.Add(category);
+            }
+
+            return list;
+        }
+
+        public Category GetCategoryByID(int id)
+        {
+            Category category = null;
+
+            string query = "select * from FoodCategory where id = " + id;
+
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                category = new Category(item);
+                return category;
+            }
+
+            return category;
+        }
+        public bool InsertCategory(string categoryName)
+        {
+            string query = string.Format("INSERT INTO dbo.FoodCategory (name) VALUES (N'{0}')", categoryName);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool DeleteCategory(int categoryId)
+        {
+            string query = string.Format("DELETE FROM dbo.FoodCategory WHERE ID = {0}", categoryId);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool UpdateCategory(int id, string name)
+        {
+            string query = string.Format("UPDATE dbo.FoodCategory SET name = N'{0}' WHERE id = {1}", name, id);
+            int result = DataProvider.Instance.ExecuteNonQuery(query);
+
+            return result > 0;
+        }
+    }
+}
---
+++ QuanLyQuanCafe/DAO/TableDAO.cs
@@ -0,0 +1,54 @@
+﻿using QuanLyQuanCafe.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCafe.DAO
+{
+    public class TableDAO
+    {
+        private static TableDAO instance;
+
+        public static TableDAO Instance
+        {
+            get { if (instance == null) instance = new TableDAO(); return TableDAO.instance; }
+            private set { TableDAO.instance = value; }
+        }
+
+        public static int TableWidth = 90;
+        public static int TableHeight = 90;
+
+        private TableDAO() { }
+
+        public void SwitchTable(int id1, int id2)
+        {
+            DataProvider.Instance.ExecuteQuery("USP_SwitchTabel @idTable1 , @idTabel2", new object[]{id1, id2});
+        }
+
+        public List<Table> LoadTableList()
+        {
+            List<Table> tableList = new List<Table>();
+
+            DataTable data = DataProvider.Instance.ExecuteQuery("USP_GetTableList");
+
+            foreach (DataRow item in data.Rows)
+            {
+                Table table = new Table(item);
+                tableList.Add(table);
+            }
+
+            return tableList;
+        }
+        public void UpdateTable(int tableId, String newStatus)
+        {
+            string query = "UPDATE TableFood SET status = @newStatus WHERE id = @tableId";
+            object[] parameters = new object[] { newStatus, tableId };
+            int rowsAffected = DataProvider.Instance.ExecuteNonQuery(query, parameters);
+            
+        }
+    }
+}
